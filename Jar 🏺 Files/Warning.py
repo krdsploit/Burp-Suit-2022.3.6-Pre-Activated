a92eb4b8f4f98de65ede71d778f5ec2ce2174e6d
krdsploit <104392085+krdsploit@users.noreply.github.com>
2022-05-03 20:29:59 -0700

    Create Warning.py

---
+++ Jar 🏺 Files/Warning.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Read it 
+
+def notice():
+  print(" You have to installing the java 10 to work with it then remove old version of burp suit and java ");
+  
+  
+  
+  
+notice()
