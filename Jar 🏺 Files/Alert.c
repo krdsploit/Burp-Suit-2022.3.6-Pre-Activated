f564f32efd83ef422a01d62d3b127191d7281e0a
krdsploit <104392085+krdsploit@users.noreply.github.com>
2022-05-03 20:39:27 -0700

    Create Alert.c

---
+++ Jar 🏺 Files/Alert.c
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "There are no malware and ransomware from this mirror download link ! you can search for each section mirrors";
+  return 0;
+}
